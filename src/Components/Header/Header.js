f49e3b0db9ba2541b7c62a6161ed371cd3199be5
elamahpla <theamazingness@gmail.com>
2018-07-21 14:19:24 -0500

    Working on header.

--- src/Components/Header/Header.js
+++ src/Components/Header/Header.js
@@ -6,7 +6,10 @@ const Header = () => (
     <NavLink className="Header-Home" to="/">Home</NavLink>
     <NavLink className="Header-About" to="/about">About</NavLink>
     <NavLink className="Header-Contact" to="/contact">Contact</NavLink>
-    <NavLink className="Header-Portfolio" to="/portfolio">Portfolio</NavLink>
+    <NavLink className="Header-Portfolio" to="/portfolio">
+      <span className="Header-Portfolio-Visible">Portfolio</span>
+      <span className="Header-Portfolio-Invisible">Portfolio</span>
+    </NavLink>
     {/* <NavLink to="/portfolio">Portfolio</NavLink>
     <NavLink to="/">Home</NavLink>
     <NavLink to="/">Home</NavLink> */}
