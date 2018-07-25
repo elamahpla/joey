56888b63ad6cae6f71ab64f88ac4607d3171eb66
elamahpla <theamazingness@gmail.com>
2018-07-25 15:30:56 -0500

    Working on Contact.js & _contact.scss.

--- src/Pages/Contact/Contact.js
+++ src/Pages/Contact/Contact.js
@@ -4,8 +4,21 @@ const Contact = () => (
   <div className="Contact">
     From Contact
     <div className="Contact-GitHub">
-      <p><a href="https://github.com/elamahpla">GitHub<br/></a>
+      <p>
+        <a href="https://github.com/elamahpla">GitHub<br/></a>
       has been an amazing tool for me. 
+      </p>
+    </div>
+    <div className="Contact-Twitter">
+      <p>
+        <a href="https://twitter.com/joeyrobinsondev">Twitter<br/></a>
+        For all the Tweets I send /s
+      </p>
+    </div>
+    <div className="Contact-LinkedIn">
+      <p>
+        <a href="https://www.linkedin.com/in/joey-robinson/">LinkedIn<br/></a>
+        It's LinkedIn
       </p>
     </div>
   </div>
