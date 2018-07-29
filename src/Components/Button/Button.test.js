104ab6baca426d963569d156dc6c098a91d83793
elamahpla <theamazingness@gmail.com>
2018-07-29 02:07:21 -0500

    I hope this works.

---
+++ src/Components/Button/Button.test.js
@@ -0,0 +1,5 @@
+// import Button from './Button';
+
+// test('Renders button with Props as className', () => {
+//   expect(sum(1, 2)).toBe(3);
+// });
--- src/Pages/Contact/Contact.js
+++ src/Pages/Contact/Contact.js
@@ -3,32 +3,16 @@ import React from 'react';
 const Contact = () => (
   <div className="Contact">
     <div className="Contact-GitHub">
-      <p>
         <a target="_blank" href="https://github.com/elamahpla">GitHub<br/></a>
-      <ul>
-        <li>Great</li>
-        <li>Amazing</li>
-        <li>Invaluable</li>
-      </ul>
-      </p>
     </div>
     <div className="Contact-Twitter">
-      <p>
         <a target="_blank" href="https://twitter.com/joeyrobinsondev">Twitter<br/></a>
-        I don't really Tweet too often. Feel free to @ me, though.
-      </p>
     </div>
     <div className="Contact-LinkedIn">
-      <p>
         <a target="_blank" href="https://www.linkedin.com/in/joey-robinson/">LinkedIn<br/></a>
-        It's LinkedIn
-      </p>
     </div>
     <div className="Contact-Email">
-      <p>
         <a target="_blank" href="mailto:joeyrobinsondev@gmail.com">Email<br/></a>
-        Send me an Email
-      </p>
     </div>
   </div>
 );
