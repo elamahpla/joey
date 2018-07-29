8ddb9b5293a12404224d8339a2a0897a5025a1c2
elamahpla <theamazingness@gmail.com>
2018-07-29 13:05:25 -0500

    Updating _skills.scss and Skills.js.

--- src/Pages/Skills/Skills.js
+++ src/Pages/Skills/Skills.js
@@ -15,6 +15,17 @@ const Skills = () => (
         <li>Mobile Friendly Websites</li>
         <li>Fully Responsive Websites</li>
       </ul>
+      <br/>
+    </div>
+    <h1 className="Skills-WorkingOn">Working On:</h1>
+    <div className="Skills-CSS-WorkingOn">
+      <ul>
+        <li>BEM & Atomic Architecture</li>
+        <li>Better Design Principles</li>
+        <li>Better Flow Of&nbsp; CSS</li>
+        <li>Better Use of CSS Grid</li>
+        <li>Usage of Mixins & Functions</li>
+      </ul>
     </div>
     <div className="Skills-JavaScript">
       <img id="one" src="https://raw.githubusercontent.com/voodootikigod/logo.js/master/js.png" alt=""/>
@@ -26,14 +37,31 @@ const Skills = () => (
         <li>fetch</li>
       </ul>
     </div>
+    <div className="Skills-JavaScript-WorkingOn">
+      <ul>
+        <li>Learning JS Better In General</li>
+        <li>Fully Understand How & Why</li>
+        <li>Deconstructing</li>
+        <li>Handling Input Changes</li>
+        <li>Understanding API Calls Better</li>
+      </ul>
+    </div>
     <div className="Skills-React">
       <img id="one" src="https://drive.google.com/uc?id=1FBgXocWb_IzykA0i5urkOgVwXN2XJ1IO" alt=""/>
       <ul>
         <li>Maintaining Efficient Workflow</li>
         <li>Creating Reusable Components</li>
         <li>React Router</li>
-        <li>Jest</li>
         <li>GatsbyJS</li>
+      </ul>
+    </div>
+    <div className="Skills-React-WorkingOn">
+      <ul>
+        <li>Understanding React Better In General</li>
+        <li>Jest & Testing In General</li>
+        <li>Using State More Effectively</li>
+        <li>Sever Side Rendering (SSR)</li>
+        <li>Reach Router</li>
       </ul>
     </div>
     <div className="Skills-Other">
@@ -47,12 +75,30 @@ const Skills = () => (
         <li>Command Line</li>
       </ul>
     </div>
+    <div className="Skills-Other-WorkingOn">
+      <ul>
+        <li>Using Webpack Better</li>
+        <li>Having Better NPM Script Usage</li>
+        <li>Using NPM Scripts To Compile SCSS</li>
+        <li>Learning And Knowing Gulp And Grunt</li>
+        <li></li>
+      </ul>
+    </div>
     <div className="Skills-Soft">
       <ul>
         <li>Communication</li>
         <li>Problem Solving</li>
         <li>Teamwork</li>
         <li>Time Management</li>
+      </ul>
+    </div>
+    <div className="Skills-Soft-WorkingOn">
+      <ul>
+        <li>Learning When To Stop Obsessing Over An Issue</li>
+        <li>Effectively Explaining Myself</li>
+        <li>Knowing When To Ask For Help</li>
+        <li>Better Problem Solving</li>
+        <li>Living A Healthier Life</li>
       </ul>
     </div>
   </div>
